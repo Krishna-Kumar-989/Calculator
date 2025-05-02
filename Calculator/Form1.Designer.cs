b6fce3c6e1af6113aeaeb49e14f69ed3c2022af1
Krishna-Kumar-989 <45414142+Krishna-Kumar-989@users.noreply.github.com>
2025-05-02 22:35:30 +0530

    ,

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btndiv = new System.Windows.Forms.Button();
+            this.btnce = new System.Windows.Forms.Button();
+            this.btnc = new System.Windows.Forms.Button();
+            this.btnmul = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btnsub = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnequalto = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnpoint = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.labelCurrentOperation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            resources.ApplyResources(this.btn7, "btn7");
+            this.btn7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn7.Name = "btn7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn8
+            // 
+            resources.ApplyResources(this.btn8, "btn8");
+            this.btn8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn8.Name = "btn8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn9
+            // 
+            resources.ApplyResources(this.btn9, "btn9");
+            this.btn9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn9.Name = "btn9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btndiv
+            // 
+            resources.ApplyResources(this.btndiv, "btndiv");
+            this.btndiv.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btndiv.Name = "btndiv";
+            this.btndiv.UseVisualStyleBackColor = true;
+            this.btndiv.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btnce
+            // 
+            resources.ApplyResources(this.btnce, "btnce");
+            this.btnce.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnce.Name = "btnce";
+            this.btnce.UseVisualStyleBackColor = true;
+            this.btnce.Click += new System.EventHandler(this.btnce_Click);
+            // 
+            // btnc
+            // 
+            resources.ApplyResources(this.btnc, "btnc");
+            this.btnc.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnc.Name = "btnc";
+            this.btnc.UseVisualStyleBackColor = true;
+            this.btnc.Click += new System.EventHandler(this.btnc_Click);
+            // 
+            // btnmul
+            // 
+            resources.ApplyResources(this.btnmul, "btnmul");
+            this.btnmul.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnmul.Name = "btnmul";
+            this.btnmul.UseVisualStyleBackColor = true;
+            this.btnmul.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btn6
+            // 
+            resources.ApplyResources(this.btn6, "btn6");
+            this.btn6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn6.Name = "btn6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn5
+            // 
+            resources.ApplyResources(this.btn5, "btn5");
+            this.btn5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn5.Name = "btn5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn4
+            // 
+            resources.ApplyResources(this.btn4, "btn4");
+            this.btn4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn4.Name = "btn4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnsub
+            // 
+            resources.ApplyResources(this.btnsub, "btnsub");
+            this.btnsub.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnsub.Name = "btnsub";
+            this.btnsub.UseVisualStyleBackColor = true;
+            this.btnsub.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btn3
+            // 
+            resources.ApplyResources(this.btn3, "btn3");
+            this.btn3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn3.Name = "btn3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            resources.ApplyResources(this.btn2, "btn2");
+            this.btn2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn2.Name = "btn2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn1
+            // 
+            resources.ApplyResources(this.btn1, "btn1");
+            this.btn1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn1.Name = "btn1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnequalto
+            // 
+            resources.ApplyResources(this.btnequalto, "btnequalto");
+            this.btnequalto.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnequalto.Name = "btnequalto";
+            this.btnequalto.UseVisualStyleBackColor = true;
+            this.btnequalto.Click += new System.EventHandler(this.btnequalto_Click);
+            // 
+            // btnadd
+            // 
+            resources.ApplyResources(this.btnadd, "btnadd");
+            this.btnadd.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnadd.Name = "btnadd";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.operator_click);
+            // 
+            // btnpoint
+            // 
+            resources.ApplyResources(this.btnpoint, "btnpoint");
+            this.btnpoint.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnpoint.Name = "btnpoint";
+            this.btnpoint.UseVisualStyleBackColor = true;
+            this.btnpoint.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn0
+            // 
+            resources.ApplyResources(this.btn0, "btn0");
+            this.btn0.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn0.Name = "btn0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // textBox_Result
+            // 
+            resources.ApplyResources(this.textBox_Result, "textBox_Result");
+            this.textBox_Result.Name = "textBox_Result";
+            // 
+            // labelCurrentOperation
+            // 
+            resources.ApplyResources(this.labelCurrentOperation, "labelCurrentOperation");
+            this.labelCurrentOperation.Name = "labelCurrentOperation";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelCurrentOperation);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.btnequalto);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.btnpoint);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnsub);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btnc);
+            this.Controls.Add(this.btnmul);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btnce);
+            this.Controls.Add(this.btndiv);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btndiv;
+        private System.Windows.Forms.Button btnce;
+        private System.Windows.Forms.Button btnc;
+        private System.Windows.Forms.Button btnmul;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btnsub;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnequalto;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnpoint;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.Label labelCurrentOperation;
+    }
+}
+
